c81f838087da0375ee06af61cd4148cbdb007d88
Kebing Yu <kebing.yu@oath.com>
2018-09-06 23:00:55 +0000

    update update.bash

--- update.bash
+++ update.bash
@@ -1,5 +1,5 @@
 git pull
 git submodule sync    # Ensure the submodule points to the right place
-git submodule update  # Update the submodule  
 git submodule foreach git checkout master  # Ensure subs are on master branch
 git submodule foreach git pull origin master # Pull the latest master
+git submodule update
